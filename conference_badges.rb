f6d34a74424548a303f998ce94bb9f44550194a2
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 14:54:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,6 @@ end
 end
 
 def assign_rooms(name)
-  name.each_with_index do { |item, index| }
+  name.each_with_index { |item, index| }
   return "Hello, #{item}! You'll be assigned to room #{index}!"
 end
