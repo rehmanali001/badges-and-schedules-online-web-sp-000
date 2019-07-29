f4b8ce405cbe81d4cad60645375d775e22e380a9
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 14:43:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  new_array = []
+  name.each do |speaker|
+  new_array.push("Hello, my name is #{speaker}.")
+end
+  new_array
+end
